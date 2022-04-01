01332223d32e562925ed582aa76fee65f9779dd3
Lyssie <lyssiewright.15@gmail.com>
2022-03-31 20:57:45 -0500

    added code to delete patient appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -5,4 +5,20 @@ class AppointmentsController < ApplicationController
         appointment = Appointment.all
         appointment.to_json(include: :physician)
     end
+
+    post "/appointments" do
+        phys = Physician.all.sample
+        appointment = Appointment.create(
+        patient_id:params[:id],
+        physician_id:phys[:id],
+        appt_date:Date.today
+        )
+        appointment.to_json
+      end
+
+    delete "/appointments/:id" do
+        appointment = Appointment.find(params[:id])
+        appointment.destroy
+        appointment.to_json
+    end
 end
